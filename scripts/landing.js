eb368e9c31bccb34b260816c0b3cfe416e96c0bf
Eric Peterson <ericpeterson@Erics-MBP.domain>
2017-07-04 11:24:59 -0600

    Assignment 8 Revision

--- scripts/landing.js
+++ scripts/landing.js
@@ -8,7 +8,7 @@ var animatePoints = function() {
      points[index].style.WebkitTransform = "scaleX(1) translateY(0)";
    }
 
-   for (var = i; i < points.length; i++) {
+   for (var = 0; i < points.length; i++) {
      revealPoint(i);
    }
 };
