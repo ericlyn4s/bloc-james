a47bb7261862d0f7c2a0a8895b2fc2dc0551f389
Eric Peterson <ericpeterson@Erics-MacBook-Pro.local>
2017-07-05 19:12:55 -0600

    Checkpoint-9.2: completed

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,12 +1,12 @@
 var pointsArray = document.getElementsByClassName('point');
 
-var animatePoints = function(points) {
+ var animatePoints = function(points) {
    var revealPoint = function(index) {
      points[index].style.opacity = 1;
      points[index].style.transform = "scaleX(1) translateY(0)";
      points[index].style.msTransform = "scaleX(1) translateY(0)";
      points[index].style.WebkitTransform = "scaleX(1) translateY(0)";
-   }
+   };
 
    for (var i = 0; i < points.length; i++) {
      revealPoint(i);
@@ -15,15 +15,13 @@ var animatePoints = function(points) {
 
 window.onload = function() {
   // Automatically animate the points on a tall screen where scrolling can't trigger the animation
-  if (window.innerHeight > 950) {
-     animatePoints(pointsArray);
-   }
-       var sellingPoints = document.getElementsByClassName('selling-points')[0];
-       var scrollDistance = sellingPoints.getBoundingClientRect().top - window.innerHeight + 200;
+     if (window.innerHeight > 950) {
+         animatePoints(pointsArray);
+     }
 
   window.addEventListener('scroll', function(event) {
-    if (document.documentElement.scrollTop || document.body.scrollTop >= scrollDistance) {
-          animatePoints(pointsArray);
-      }
+    if (pointsArray[0].getBoundingClientRect().top <= 500) {
+        animatePoints(pointsArray);
+    }
   });
 }
