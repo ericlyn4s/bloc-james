9abff3e3a6dadae6a1eddb2ef04e490a906335ed
Eric Peterson <ericpeterson@Erics-MacBook-Pro.local>
2017-07-08 12:18:44 -0600

    Assignment-10.5

--- scripts/landing.js
+++ scripts/landing.js
@@ -2,7 +2,7 @@ var pointsArray = document.getElementsByClassName('point');
 
 var animatePoints = function(points) {
     var revealPoint = function(index) {
-        point[index].style.opacity=1;
+        points[index].style.opacity=1;
         points[index].style.transform = "scaleX(1) translateY(0)";
         points[index].style.msTransform = "scaleX(1) translateY(0)";
         points[index].style.WebkitTransform = "scaleX(1) translateU(0)";
