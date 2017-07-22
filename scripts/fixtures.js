b47733c6a4ea8e3e5a50d1074e2af1e2acbe1184
Eric Peterson <ericpeterson@Erics-MacBook-Pro.local>
2017-07-22 14:46:36 -0600

    Checkpoint 19 Completed

--- scripts/fixtures.js
+++ scripts/fixtures.js
@@ -8,12 +8,11 @@ var albumPicasso = {
         { title: 'Blue', duration: '4:26' },
         { title: 'Green', duration: '3:14' },
         { title: 'Red', duration: '5:01' },
-        { title: 'Pink', duration: '3:21'},
-        { title: 'Magenta', duration: '2:15'}
+        { title: 'Pink', duration: '3:21' },
+        { title: 'Magenta', duration: '2:15' }
     ]
 };
 
-// Another Example Album
 var albumMarconi = {
     title: 'The Telephone',
     artist: 'Guglielmo Marconi',
@@ -23,8 +22,8 @@ var albumMarconi = {
     songs: [
         { title: 'Hello, Operator?', duration: '1:01' },
         { title: 'Ring, ring, ring', duration: '5:01' },
-        { title: 'Fits in your pocket', duration: '3:21'},
+        { title: 'Fits in your pocket', duration: '3:21' },
         { title: 'Can you hear me now?', duration: '3:14' },
-        { title: 'Wrong phone number', duration: '2:15'}
+        { title: 'Wrong phone number', duration: '2:15' }
     ]
 };
